c5db4850c71764894f8d3610894c9c1afe4c7202
dang-dev <aljohnvillacruel141@gmail.com>
2022-05-27 18:32:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Home from "./components/home";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import SignIn from "./Auth/Login";
+import { RequireAuth } from "./Auth/RequiredAuth";
+import Admin from "./components/Admin";
+import {
+  onAuthStateChanged
+} from 'firebase/auth'
+import { auth } from './firebase'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Box sx={{ height: "100vh" }}>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route path="/sign-in" element={<SignIn />} /> 
+            <Route
+              path="/admin"
+              element={
+                <>
+                  <RequireAuth>
+                    <Admin />
+                  </RequireAuth>
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <Home />
+                </>
+              }
+            />
+          </Routes>
+        </UserAuthContextProvider>
+      </Box>
+    </React.Fragment>
   );
 }
 
